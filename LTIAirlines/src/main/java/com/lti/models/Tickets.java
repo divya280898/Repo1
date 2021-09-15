7118e79f1786be35c8af43ae0ce2225ffdcf7b18
KHUSHI AGARWAL <KHUSHI AGARWAL@192.168.1.101>
2021-09-15 12:46:18 +0530

    commit from Khushi

---
+++ LTIAirlines/src/main/java/com/lti/models/Tickets.java
@@ -0,0 +1,11 @@
+package com.lti.models;
+
+public class Tickets {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("test sampple for pushing and pulling in git");
+
+	}
+
+}
