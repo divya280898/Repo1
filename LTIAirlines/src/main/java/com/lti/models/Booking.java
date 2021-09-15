9e2776aa6856bd192c4e02216170585619db5678
divya280898 <divyasingh280898@gmail.com>
2021-09-15 12:14:15 +0530

    This is Booking

---
+++ LTIAirlines/src/main/java/com/lti/models/Booking.java
@@ -0,0 +1,21 @@
+package com.lti.models;
+
+public class Booking {
+	int bookingNo;
+	Flight flight;
+	public int getBookingNo() {
+		return bookingNo;
+	}
+	public void setBookingNo(int bookingNo) {
+		this.bookingNo = bookingNo;
+	}
+	public Flight getFlight() {
+		return flight;
+	}
+	public void setFlight(Flight flight) {
+		this.flight = flight;
+	}
+	
+	}
+	
+
